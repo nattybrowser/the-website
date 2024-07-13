95e4c98377f4caa8eb9eb74346fd59d68830c32d
nattybrowser <nattyskabshe@gmail.com>
2024-07-13 14:13:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,96 +1,39 @@
 let postsData = "";
 const postsContainer = document.querySelector(".posts-container");
 const searchDisplay = document.querySelector(".search-display");
+const postsPerPage = 9;
+let currentPage = 1;
 
-fetch(
-  "natty.json"
-).then(async (response) => {
+// Fetch data and display initial 9 posts
+fetch("natty.json").then(async (response) => {
   postsData = await response.json();
-  postsData.map((post) => createPost(post));
+  displayPosts(1);
 });
 
-const createPost = (postData) => {
-  const { title, link, image, categories } = postData;
-  const post = document.createElement("div");
-  post.className = "post";
-  post.innerHTML = `
-      <a class="post-preview" href="${link}">
-        <img class="post-image" src="${image}" >
-      </a>
-      <div class="post-content">
-        <p class="post-title">${title}</p>
-        <div class="post-tags">
-          ${categories
-            .map((category) => {
-              return '<span class="post-tag">' + category + "</span>";
-            })
-            .join("")}
-        </div>
-      </div>
-  `;
-
-  postsContainer.append(post);
-};
-
-const handleSearchPosts = (query) => {
-  const searchQuery = query.trim().toLowerCase();
+const displayPosts = (page) => {
+  postsContainer.innerHTML = "";
   
-  if (searchQuery.length <= 1) {
-    resetPosts()
-    return
-  }
+  const startIndex = (page - 1) * postsPerPage;
+  const endIndex = page * postsPerPage;
+  const currentPosts = postsData.slice(startIndex, endIndex);
   
-  let searchResults = [...postsData].filter(
-    (post) =>
-      post.categories.some((category) => category.toLowerCase().includes(searchQuery)) ||
-      post.title.toLowerCase().includes(searchQuery)
-  );
+  currentPosts.map((post) => createPost(post));
   
-  if (searchResults.length == 0) {
-    searchDisplay.innerHTML = "No results found"
-  } else if (searchResults.length == 1) {
-    searchDisplay.innerHTML = `1 result found for your query: ${query}`
+  if (endIndex >= postsData.length) {
+    document.querySelector(".seemore").style.display = "none";
   } else {
-    searchDisplay.innerHTML = `${searchResults.length} results found for your query: ${query}`
+    document.querySelector(".seemore").style.display = "block";
   }
-
-  postsContainer.innerHTML = "";
-  searchResults.map((post) => createPost(post));
 };
 
-const resetPosts = () => {
-  searchDisplay.innerHTML = ""
-  postsContainer.innerHTML = "";
-  postsData.map((post) => createPost(post));
+const createPost = (postData) => {
+  // Same as your original createPost function
 };
 
-const search = document.getElementById("search");
-
-let debounceTimer;
-const debounce = (callback, time) => {
-  window.clearTimeout(debounceTimer);
-  debounceTimer = window.setTimeout(callback, time);
+const handleSeeMore = () => {
+  currentPage++;
+  displayPosts(currentPage);
 };
 
-search.addEventListener(
-  "input",
-  (event) => {
-    const query = event.target.value;
-    debounce(() => handleSearchPosts(query), 500);
-  },
-  false
-);
-
-
-
-window.addEventListener("load", function () {
-    const preloader = document.getElementById("preloader");
-    preloader.classList.add("hide-preloader");
-
-    // Initialize Isotope
-    $('.entry-container').isotope({
-      itemSelector: '.entry-item',
-      layoutMode: 'masonry'
-    });
-    
-  });
+const seeMoreButton = document.querySelector(".seemore");
+seeMoreButton.addEventListener("click", handleSeeMore);
